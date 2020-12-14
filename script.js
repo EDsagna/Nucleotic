c1597f86e2e1f2ec47df84f0f7f4d23efa954ed9
sagna1 <tom.selleck@hotmail.com>
2020-12-14 11:42:11 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+// dom selection
+
+/*const list = 'element #list'
+const input = 'element #input'
+const add = 'element #add'
+const clear = 'element #clear'
+const url = 'element #url'
+const load = 'element #load'
+
+/*on recupere le tableau des taches deja existantes*/
+
+
